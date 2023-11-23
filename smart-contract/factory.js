3b72dbad86345660aa01df1d5ddc12f16daf8c41
Satyajit Ghosh <satyajit.ghosh@outlook.in>
2023-11-23 23:38:01 +0530

    smart contract updated

--- smart-contract/factory.js
+++ smart-contract/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from "./build/CampaignFactory.json";
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  "0x0A55a2fbcd722d4C07333E092147737B37Fe3c6A"
+  "0x68F6896FEAa6216Bda358cf1880af34aCf0d1784"
 );
 
 export default instance;
